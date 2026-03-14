f324242f3a1bcb32e82e22a11daaddbc3d444932
mohamed <mohamedhussein6500@gmail.com>
2026-03-14 03:24:21 +0300

    setup database

---
+++ db/connect.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const connectDB = (url)=>{
+    return mongoose.connect(url)
+    .then (()=>console.log('CONNECTED TO THE DATABASE'))
+    .catch((err)=> console.log(err))
+}
+
+module.exports = connectDB
